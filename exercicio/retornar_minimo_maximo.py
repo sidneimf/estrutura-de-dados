70a7c2cdc57af009a89a5c4f26f11b59c07ae6f4
sidneimf <sidnei.mfrancisco@gmail.com>
2021-02-25 20:29:35 -0300

    exercicio da aula 25/02/2021

--- exercicio/retornar_minimo_maximo.py
+++ exercicio/retornar_minimo_maximo.py
@@ -1,16 +1,21 @@
 """
-Verificação do código
+    Verificação do código
 
->>> lista = _gerar_lista_teste()
->>> lista
-[345, 1407, 60, 981, 1639, 1671, 60, 1074, 1698, 432, 776, 1494, 1332, 888, 205, 1042, 494, 398, 962, 1712, 931, 16, 1619, 326, 1513, 1927, 1898, 81, 206, 554]
+    >>> lista = _gerar_lista_teste()
 
->>> retornar_maior_menor(lista)
-(1927, 16)
+    >>> lista
+    [345, 1407, 60, 981, 1639, 1671, 60, 1074, 1698, 432, 776, 1494, 1332, 888, 205, 1042, 494, 398, 962, 1712, 931, 16, 1619, 326, 1513, 1927, 1898, 81, 206, 554]
+
+    >>> tamanho = len(lista)
+
+    >>> maior = menor = None
+
+    >>> retornar_maior_menor(lista, tamanho, maior, menor)
+    (1927, 16)
 """
 
 
-def gerar_lista_aleatoria(quantidade,maximo):
+def gerar_lista_aleatoria(quantidade, maximo):
     import random
     lista = []
     for i in range(quantidade):
@@ -21,16 +26,19 @@ def _gerar_lista_teste():
     lista = [345, 1407, 60, 981, 1639, 1671, 60, 1074, 1698, 432, 776, 1494, 1332, 888, 205, 1042, 494, 398, 962, 1712, 931, 16, 1619, 326, 1513, 1927, 1898, 81, 206, 554]
     return lista
 
-def retornar_maior_menor(lista):
-    maior = menor = lista[0]
-    for item in lista[1:]:
-        if item < menor:
-            menor = item
+def retornar_maior_menor(lista, tamanho, maior, menor):
+    item = lista[tamanho - 1]
+    if menor == None or item < menor:
+        menor = item
 
-        if item > maior:
-            maior = item
+    if maior == None or item > maior:
+        maior = item
 
-    return maior, menor
+    tamanho -= 1
+
+    if tamanho > -1:
+        maior, menor = retornar_maior_menor(lista, tamanho, maior, menor)
+    return (maior, menor)
 
 
 if __name__ == '__main__':
@@ -38,6 +46,9 @@ if __name__ == '__main__':
     doctest.testmod()
 
     lista = gerar_lista_aleatoria(30, 2000)
-    print(lista)
-    maior, menor = retornar_maior_menor(lista)
-    print (f'maior = {maior} e menor = {menor}')
+    tamanho = len(lista)
+    if tamanho > 0:
+        maior = menor = lista[0]
+        print(lista)
+        maior, menor = retornar_maior_menor(lista,tamanho, maior, menor)
+        print (f'maior = {maior} e menor = {menor}')
